3132a11bb5fd2b8f468488c9ecd7b07b756cd04e
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-08-11 12:28:15 +0300

    Task 18 complete. test ok.

--- Example_02/Program.cs
+++ Example_02/Program.cs
@@ -3,4 +3,26 @@
 возможных координат точек в этой четверти (x и y).
  */
 
- 
+Console.WriteLine("Введите номер плоскости от 1 до 4");
+int X = int.Parse(Console.ReadLine());
+
+if (X == 1)
+{
+    Console.WriteLine("Диапазон координат X>0;Y>0");
+}
+else if (X == 2)
+{
+    Console.WriteLine("Диапазон координат X<0;Y>0");
+}
+else if (X == 3)
+{
+    Console.WriteLine("Диапазон координат X<0;Y<0");
+}
+else if (X == 4)
+{
+    Console.WriteLine("Диапазон координат X>0;Y<0");
+}
+else if (X < 1 || X > 4)
+{
+    Console.WriteLine("Нет координат для данной плоскости");
+}
