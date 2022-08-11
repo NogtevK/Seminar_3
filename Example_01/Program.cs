5b6db858cf0becdffe18513216fe0eb7be510296
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-08-11 11:45:40 +0300

    create a programm in the cordinate system. Test OK!

---
+++ Example_01/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 17. Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+номер четверти плоскости, в которой находится эта
+точка. */
+
+Console.WriteLine("Введите X:");
+int X = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Y:");
+int Y = int.Parse(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("Первая плоскость");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Вторая плоскость");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Третья плоскость");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Четвёртая плоскость");
+}else
+{
+    Console.WriteLine("Не принадлежит ни одной плоскости");
+}
